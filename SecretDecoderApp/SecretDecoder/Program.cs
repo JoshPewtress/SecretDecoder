4c1636be9f47236f615deb1316f7d744b2c26b25
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-29 12:41:13 -0600

    feat: add Decrypt method

--- SecretDecoderApp/SecretDecoder/Program.cs
+++ SecretDecoderApp/SecretDecoder/Program.cs
@@ -7,8 +7,16 @@ public static class Encrypter
     public static string Encrypt(string input)
     {
         var encoded = input.Select((c, i) =>
-        (char)(c + ( 2 * ( (i + i + 1) - 2 ) ))).ToList();
+        (char)(c + ( 2 * ( i + 1 ) ) )).ToList();
 
         return string.Join("", encoded);
+    }
+
+    public static string Decrypt(string input)
+    {
+        var decoded = input.Select((c, i) =>
+        (char)( c - ( 2 * ( i + 1 ) ) )).ToList();
+
+        return string.Join("", decoded);
     }
 }
