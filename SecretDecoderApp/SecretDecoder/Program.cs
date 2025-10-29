9661b3a242d74693e79e484e093b0ed90d49e93e
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-29 12:27:26 -0600

    feat: add Encrypter with Encrypt method

--- SecretDecoderApp/SecretDecoder/Program.cs
+++ SecretDecoderApp/SecretDecoder/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+
+
+
+public static class Encrypter
+{
+    public static string Encrypt(string input)
+    {
+        var encoded = input.Select((c, i) =>
+        (char)(c + ( 2 * ( (i + i + 1) - 2 ) ))).ToList();
+
+        return string.Join("", encoded);
+    }
+}
